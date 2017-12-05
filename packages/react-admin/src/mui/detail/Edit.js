b4ea344816e241a4ab188d603cb3e6be89870af3
gildas <gildas.garcia@gmail.com>
2017-12-05 08:15:38 +0100

    [WIP] Review permissions

--- packages/react-admin/src/mui/detail/Edit.js
+++ packages/react-admin/src/mui/detail/Edit.js
@@ -13,7 +13,6 @@ import {
 } from '../../actions/dataActions';
 import DefaultActions from './EditActions';
 import translate from '../../i18n/translate';
-import withChildrenAsFunction from '../withChildrenAsFunction';
 
 /**
  * Page component for the Edit view
@@ -219,8 +218,7 @@ const enhance = compose(
         crudGetOne: crudGetOneAction,
         crudUpdate: crudUpdateAction,
     }),
-    translate,
-    withChildrenAsFunction
+    translate
 );
 
 export default enhance(Edit);
--- packages/react-admin/src/mui/detail/Show.js
+++ packages/react-admin/src/mui/detail/Show.js
@@ -10,7 +10,6 @@ import Title from '../layout/Title';
 import { crudGetOne as crudGetOneAction } from '../../actions/dataActions';
 import DefaultActions from './ShowActions';
 import translate from '../../i18n/translate';
-import withChildrenAsFunction from '../withChildrenAsFunction';
 
 /**
  * Page component for the Show view
@@ -174,8 +173,7 @@ function mapStateToProps(state, props) {
 
 const enhance = compose(
     connect(mapStateToProps, { crudGetOne: crudGetOneAction }),
-    translate,
-    withChildrenAsFunction
+    translate
 );
 
 export default enhance(Show);
